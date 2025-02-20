f1722f94210af4b8d7bdf7f9b715153eb8252a89
chogene <cho.eugenelee@gmail.com>
2025-02-20 15:10:40 +0900

    working SOC vs Voltage plot

--- SOC_graph.m
+++ SOC_graph.m
@@ -8,54 +8,73 @@ T.Properties.VariableNames = {...
     'VAR1', 'VAR2', 'VAR3', 'VAR4', 'VAR5', 'VAR6', 'VAR7', 'VAR8', 'VAR9', 'VAR10',...
     'VAR11', 'VAR12', 'VAR13', 'VAR14', 'VAR15'};
 
-removeFirstCycle = (T.CycleC == 1);
-T(removeFirstCycle, :) = [];
+% First cycle is for adjusting the battery
+ignoreFirstCycle= (T.CycleC == 1);
+T(ignoreFirstCycle, :) = [];
 
-T2 = T(T.CycleC == 3, :);
-% T3 = T(T.CycleC == 3, :);
+% Need to add function to check through all loops
+% Checking SOC of cycle n
+T_n = T(T.CycleC == 4, :);
 
-checkDischarge = strcmp(T2.Mode, 'D');
-checkCharge = strcmp(T2.Mode, 'C');
+% strcmp: string compare
+% checkRest = strcmp(T_n.Mode, 'R');
+% T_n = T_n(~checkRest, :);
+checkDischarge = strcmp(T_n.Mode, 'D');
+checkCharge = strcmp(T_n.Mode, 'C');
 
-signedCurrent = zeros(height(T2), 1);
-signedCurrent(checkCharge) = +T2.Current_A(checkCharge);
-signedCurrent(checkDischarge) = -T2.Current_A(checkDischarge);
+% Making sure current has proper sign, program calculates absolute value
+signedCurrent = zeros(height(T_n), 1);
+signedCurrent(checkCharge) = +T_n.Current_A(checkCharge);
+signedCurrent(checkDischarge) = -T_n.Current_A(checkDischarge);
 
-nominalCapacity = 10; % [Ah]
-initSOC = 0; % [%] 
+% Battery specifications 
+nominalCapacity = 9.90277; % [Ah] 
+% Average capacity [Ah] for the final three cycles bc first is obviously ignored 
+% and the second cylce isn't considered since it's assumed that it is "unstable" and doesn't represent the physical state of the battery
+% The battery fluctuates because of overpotential and chemical stuff.
+initSOC = 99.999999; % [%] 
 
-time_T2 = T2.TestTime_h / 60;
+% Resetting time to start at cycle loop 
+time_T_n = T_n.TestTime_h / 60;
 
-% calculating absolute time?
-time0 = time_T2 - time_T2(1);
-dt = [0; -diff(time0)];
+% Time starts at cycle
+time0 = time_T_n - time_T_n(1);
 
-% calculate SOC
-SOC = zeros(height(T2), 1);
+% Timestep
+dt = [0; diff(time0)];
+
+% Calculate SOC
+SOC = zeros(height(T_n), 1);
 SOC(1) = initSOC;
 
-for i = 2:height(T2)
+for i = 2:height(T_n)
     dQ = signedCurrent(i) * (dt(i));
-    dSOC_percent = (dQ / nominalCapacity) * 100;
-    SOC(i) = SOC(i-1) + dSOC_percent;
+    dSOC = (dQ / nominalCapacity) * 100;
+    SOC(i) = SOC(i-1) + dSOC;
 end
 
-figure('Name', '001 CCCV', 'NumberTitle', 'off');
-yyaxis left;
-plot(time_T2, T2.Voltage_V, 'b-');
-xlabel('Time [h]');
+figure('Name', '001 Cycle 4', 'NumberTitle', 'off');
+plot(time0, T_n.Voltage_V, 'b');
 ylabel('Voltage [V]');
-xlim([21.5942 25.66]);
 hold on;
 
 yyaxis right;
-plot(time_T2, T2.Current_A, 'r');
+plot(time0, T_n.Current_A, 'r');
+xlabel('Time (hours)');
 ylabel('Current [A]');
+hold off;
 grid on;
 
 figure('Name', '001 SOC', 'NumberTitle', 'off');
-plot(SOC, T2.Voltage_V, 'r-');
-xlabel('SOC [%]');
-ylabel('Voltage [V}');
-xlim([0 100]);
+hold on;
 grid on;
+
+plot(SOC(checkCharge), T_n.Voltage_V(checkCharge), 'r-');
+plot(SOC(checkDischarge), T_n.Voltage_V(checkDischarge), 'b-');
+
+xlabel('SOC [%]');
+ylabel('Voltage [V]');
+xlim([0 100]);
+
+legend('Charge', 'Discharge');
+
